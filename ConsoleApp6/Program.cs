66c4e515553f4f2a717c854b9ad4707b565e48f0
JJ <j.kubiak164@gmail.com>
2019-06-13 00:00:46 +0200

    Lock recursion

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static SpinLock sl = new SpinLock(true);
+        public static void LockRecursion(int x)
+        {
+            bool lockTaken = false;            
+            try
+            {
+                sl.Enter(ref lockTaken); 
+            }
+            catch (LockRecursionException e)
+            {
+                Console.WriteLine("Exception: "+ e);
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Console.WriteLine($"Took a lock, x = {x}");
+                    LockRecursion(x - 1);
+                    sl.Exit();
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to take a lock, x = {x}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LockRecursion(5);
+        }
+    }
+}
