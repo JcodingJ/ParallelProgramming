228b393ce8f0caeb3eef9ef3fd237e540e46d9a9
JJ <j.kubiak164@gmail.com>
2019-06-12 22:02:10 +0200

    ErrorHandling

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var item in ae.InnerExceptions)
+                {
+                    Console.WriteLine($"Handeled elsewhere: {item.GetType()}");
+                }
+            }
+
+        }
+
+        private static void Test()
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                throw new InvalidOperationException("cant do this") { Source = "t" };
+            });
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                throw new AccessViolationException("cant acces this!") { Source = "t2" };
+            });
+
+
+            try
+            {
+                Task.WaitAll(t, t2);
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    if (e is InvalidOperationException)
+                    {
+                        Console.WriteLine("invalid op!");
+                        return true;
+                    }
+                    else return false;
+                });
+            }
+        }
+    }
+}
