6af9130c2627df68196e01c69835b31852ccd8e6
JJ <j.kubiak164@gmail.com>
2019-06-12 19:42:21 +0200

    Add project files.

---
+++ ParallelProgramming.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming.ConsoleApp
+{
+    internal class Program
+    {
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while(i -- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => Write('.'));
+
+            var t = new Task(() => Write('?'));
+            t.Start();
+
+            Write('-');
+
+            Console.WriteLine("Main program done.");
+            Console.ReadLine();
+        }
+    }
+}
