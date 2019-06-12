f5d3ab0b5d10dc02f154bf96f07a151fa2531038
JJ <j.kubiak164@gmail.com>
2019-06-12 20:27:43 +0200

    Cancellation token

---
+++ ParallelProgramming.ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ParallelProgramming.ConsoleApp2
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            token.Register(() =>
+            {
+                Console.WriteLine("Cancelation has been requested");
+            });
+            
+            var t = new Task(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\t");
+                }
+            }, token);
+            t.Start();
+
+            Task.Factory.StartNew(() =>
+            {
+                token.WaitHandle.WaitOne();
+                Console.WriteLine("Wait handle requested, cancelation was requested");
+            });
+
+            Console.ReadKey();
+            cts.Cancel();
+
+            Console.WriteLine("App ends here");
+            Console.ReadKey();
+        }
+    }
+}
