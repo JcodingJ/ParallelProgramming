da03e18eca30cd4864c548d3a718047ac55e07c5
JJ <j.kubiak164@gmail.com>
2019-06-12 20:37:32 +0200

    Linked Cancellation tokens

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var emergency = new CancellationTokenSource();
+
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+                planned.Token, preventative.Token, emergency.Token);
+
+            Task.Factory.StartNew(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\t");
+                    Thread.Sleep(10);
+                }
+            }, paranoid.Token);
+
+
+            Console.ReadKey();
+            emergency.Cancel();
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
