e21c2c427c964d4bed1f2c3428d40cd75a0cad99
JJ <j.kubiak164@gmail.com>
2019-06-13 22:01:12 +0200

    mutex, padlocks

---
+++ ParallelProgramming.LockSlim/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ParallelProgramming.LockSlim
+{
+    class Program
+    {
+        static ReaderWriterLockSlim padlock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    //padlock.EnterReadLock();
+                    padlock.EnterUpgradeableReadLock();
+
+                    if(i%2 == 0)
+                    {
+                        padlock.EnterWriteLock();
+                        x = 123;
+                        padlock.ExitWriteLock();
+                    }
+
+                    Console.WriteLine($"Entered read lock x = {x}");
+                    Thread.Sleep(3000);
+
+                    //padlock.ExitReadLock();
+                    padlock.ExitUpgradeableReadLock();
+                    
+                    Console.WriteLine($"Exited read lock, x = {x}.");
+                }));
+            }
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    Console.WriteLine(e);
+                    return true;
+                });
+            }
+
+            while (true)
+            {
+                Console.ReadKey();
+                padlock.EnterWriteLock();
+                Console.WriteLine("Write lock acquired");
+                int newValue = random.Next(10);
+                x = newValue;
+                Console.WriteLine($"Set x = {x}");
+                padlock.ExitWriteLock();
+                Console.WriteLine("padlock has been released");
+            }
+        }
+    }
+}
---
+++ ParallelProgramming.Mutex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming.Mutex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string appName = "MyApp";
+            Mutex mutex;
+            try
+            {
+                mutex = Mutex.OpenExisting(appName);
+                Console.WriteLine($"Sorry, {appName} is already running");
+            }
+            catch (WaitHandleCannotBeOpenedException e)
+            {
+                Console.WriteLine("We can run program just fine");
+                mutex = new Mutex(false, appName);
+            }
+            Console.ReadKey();
+            mutex.ReleaseMutex();
+        }
+    }
+}
