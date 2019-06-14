a888d4ef5817e742b3a56371bfe1d6fb0154e583
JJ <j.kubiak164@gmail.com>
2019-06-14 20:47:15 +0200

    continuing tasks

---
+++ ParallelProgramming.Continuations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming.Continuations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Boiling water");
+            });
+
+            var task2 = task.ContinueWith(t =>
+            {
+                Console.WriteLine($"Completed task {t.Id}, pour water into cup.");
+            });
+
+            task2.Wait();
+        }
+    }
+}
