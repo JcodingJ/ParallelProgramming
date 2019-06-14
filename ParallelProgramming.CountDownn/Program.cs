76d788607a242e747ad4bc7622bf622d5dec2e3c
JJ <j.kubiak164@gmail.com>
2019-06-14 21:40:28 +0200

    countdown 1

---
+++ ParallelProgramming.CountDownn/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ParallelProgramming.CountDownn
+{
+    class Program
+    {
+        private static int tasksCount = 5;
+        static CountdownEvent cte = new CountdownEvent(tasksCount);
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
