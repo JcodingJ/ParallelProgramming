9e0313ff38acc68f8095c6d4c4eef34a3b9aea46
JJ <j.kubiak164@gmail.com>
2019-06-14 20:52:41 +0200

    continuing tasks2

--- ParallelProgramming.Continuations/Program.cs
+++ ParallelProgramming.Continuations/Program.cs
@@ -10,17 +10,18 @@ namespace ParallelProgramming.Continuations
     {
         static void Main(string[] args)
         {
-            var task = Task.Factory.StartNew(() =>
-            {
-                Console.WriteLine("Boiling water");
-            });
+            var task1 = Task.Factory.StartNew(() => "Task 1");
+            var task2 = Task.Factory.StartNew(() => "Task 2");
 
-            var task2 = task.ContinueWith(t =>
-            {
-                Console.WriteLine($"Completed task {t.Id}, pour water into cup.");
-            });
-
-            task2.Wait();
+            var task3 = Task.Factory.ContinueWhenAny(new[] { task1, task2 },
+                t=>
+                {
+                    Console.WriteLine("Task has been completed:");
+                    //foreach (var t in tasks)
+                        Console.WriteLine(" - " + t.Result);
+                    Console.WriteLine("All tasks done");
+                });
+            task3.Wait();
         }
     }
 }
