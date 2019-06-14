3fd77d97c3d12a8eb7300be2fe2cfab93866ba31
JJ <j.kubiak164@gmail.com>
2019-06-14 18:32:40 +0200

    Concurent Queue

---
+++ ParallelProgramming.ConcurentQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming.ConcurentQueuee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new ConcurrentQueue<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+
+            int result;
+            if (q.TryDequeue(out result))
+                Console.WriteLine($"Removed element {result}");
+            if(q.TryPeek(out result))
+                Console.WriteLine($"Front element is {result}");
+
+        }
+    }
+}
