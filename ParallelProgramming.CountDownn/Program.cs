c6f946f8f384062c5cae6536a195e05aeb767faa
JJ <j.kubiak164@gmail.com>
2019-06-17 09:07:24 +0200

    countdown

--- ParallelProgramming.CountDownn/Program.cs
+++ ParallelProgramming.CountDownn/Program.cs
@@ -11,9 +11,27 @@ namespace ParallelProgramming.CountDownn
     {
         private static int tasksCount = 5;
         static CountdownEvent cte = new CountdownEvent(tasksCount);
+        private static Random random = new Random();
         
         static void Main(string[] args)
         {
+            for (int i = 0; i < tasksCount; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"Entering task  {Task.CurrentId}" );
+                    Thread.Sleep(random.Next(3000));
+                    cte.Signal();
+                    Console.WriteLine($"Exiting task: {Task.CurrentId}");
+                });
+            }
+            var finalTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"waiting for other tasks to complete in {Task.CurrentId}");
+                cte.Wait();
+                Console.WriteLine("All tasks completed");
+            });
+            finalTask.Wait();
         }
     }
 }
