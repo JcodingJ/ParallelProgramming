100b11c3158b982e5a0e18ec55edc6d027f234f0
JJ <j.kubiak164@gmail.com>
2019-07-01 14:33:26 +0200

    parallel linq

---
+++ ParallelProgramming.PLinq3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming.PLinq3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 20).ToArray();
+
+            var results = numbers.AsParallel().WithMergeOptions(ParallelMergeOptions.FullyBuffered)
+                .Select(x =>
+                {
+                    var result = Math.Log10(x);
+                    Console.WriteLine($"P {result}");
+                    return result;
+                });
+
+            foreach (var result in results)
+            {
+                Console.WriteLine($"C {result}");
+            }
+        }
+    }
+}
---
+++ ParallelProgramming.PLinq4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming.PLinq4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var sum = Enumerable.Range(1, 1000).Aggregate(0, (i, acc) => i + acc);
+
+            var sum = ParallelEnumerable.Range(1, 1000)
+                .Aggregate(
+                    0,
+                    (partialSum, i) => partialSum += i,
+                    (total,subTotal)=>total+=subTotal,
+                    i=>i
+                );
+
+            Console.WriteLine($"sum = {sum}");
+        }
+    }
+}
